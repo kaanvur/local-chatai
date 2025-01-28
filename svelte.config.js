24f936ba1af292c27305070a129a441e9b1b0319
Kaan Vurgun <kaan.vurgun@hubx.co>
2025-01-28 18:36:08 +0300

    Refactor Svelte configuration to use Cloudflare adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapterCf from '@sveltejs/adapter-cloudflare';
 import adapterStatic from '@sveltejs/adapter-static';
 
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
@@ -6,13 +6,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-	  adapter: adapterStatic({
-		fallback: 'index.html', // Enable SPA fallback
-		pages: 'build',
-		assets: 'build',
-		precompress: false,
-		strict: true
-	  })
+	  adapter: adapterCf(),
 	},
 	preprocess: vitePreprocess()
 };
