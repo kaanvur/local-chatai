a491e490aa561702f233b8c94e7ccfa819aea810
Kaan Vurgun <kaan.vurgun@hubx.co>
2025-01-29 11:39:43 +0300

    Add mobile detection hook and update Toaster position based on device type

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,8 +3,11 @@
 	let { children } = $props();
 	import { ModeWatcher } from 'mode-watcher';
 	import { Toaster } from '$lib/components/ui/sonner';
+	import { createMobileDetector } from '$lib/hooks/is-mobile';
+
+	const mobile = createMobileDetector();
 </script>
 
 <ModeWatcher />
-<Toaster />
+<Toaster position={$mobile ? 'top-center' : 'bottom-right'} />
 {@render children()}
